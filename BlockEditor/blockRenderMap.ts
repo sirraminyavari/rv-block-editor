7a4b11f144b187f411b41a118b8ae802de6b6744
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-06-23 18:43:09 +0430

    CodeStyle Fix

--- BlockEditor/blockRenderMap.ts
+++ BlockEditor/blockRenderMap.ts
@@ -4,12 +4,13 @@ import { Map } from 'immutable'
 import { withBlockWrapper } from './BlockWrapper'
 
 
-export const DefaultDraftBlockRenderMapWithBlockWrapper = Map ( Object.entries ( DefaultDraftBlockRenderMap.toObject () )
-    .map ( ([ key, val ]) => [ key, {
-        ...val,
-        element: withBlockWrapper ( val.element )
-    } ] )
-    .reduce ( ( acc, [ key, val ] ) => ({ ...acc, [ key ]: val }), {} )
+export const DefaultDraftBlockRenderMapWithBlockWrapper = Map (
+    Object.entries ( DefaultDraftBlockRenderMap.toObject () )
+        .map ( ([ key, val ]) => [ key, {
+            ...val,
+            element: withBlockWrapper ( val.element )
+        } ] )
+        .reduce ( ( acc, [ key, val ] ) => ({ ...acc, [ key ]: val }), {} )
 ) as DraftBlockRenderMap
 
 const blockRenderMap: DraftBlockRenderMap = DefaultDraftBlockRenderMapWithBlockWrapper.merge ( Map ({
