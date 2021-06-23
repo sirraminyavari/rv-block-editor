c80270480cfccd8d8101e0421a301e1ff4d59219
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-06-23 22:58:15 +0430

    More PlusAction Options

--- BlockEditor/PlusMenuButton/index.tsx
+++ BlockEditor/PlusMenuButton/index.tsx
@@ -5,6 +5,8 @@ import useEditorContext from 'BlockEditor/EditorContext'
 import useUiContext from 'BlockEditor/UiContext'
 import applyPlusAction from 'BlockEditor/lib/applyPlusAction'
 
+import plusActions from './plusActions'
+
 import styles from './styles.module.scss'
 
 
@@ -15,16 +17,17 @@ export interface PlusMenuProps {
 const PlusMenu: FC < PlusMenuProps > = ({ block }) => {
     const { editorState, setEditorState } = useEditorContext ()
     const { editorRef } = useUiContext ()
-    const getAction = plusAction => () => {
-        editorRef.current?.focus ()
-        setImmediate ( () => setEditorState ( applyPlusAction ( editorState, block, plusAction ) ) )
-    }
     return <Menu as = 'div'>
         <Menu.Button as = 'div'>+</Menu.Button>
         <Menu.Items className = { styles.plusMenu }>
-            <Menu.Item as = 'div' onClick = { getAction ( 'header-one' ) }>{ () => <label>Heading 1</label> }</Menu.Item>
-            <Menu.Item as = 'div' onClick = { getAction ( 'header-two' ) }>{ () => <label>Heading 2</label> }</Menu.Item>
-            <Menu.Item as = 'div' onClick = { getAction ( 'header-three' ) }>{ () => <label>Heading 3</label> }</Menu.Item>
+            { plusActions.map ( ({ label, action }) => <Menu.Item as = 'label'
+                key = { action }
+                children = { label }
+                onClick = { () => {
+                    editorRef.current?.focus ()
+                    setImmediate ( () => setEditorState ( applyPlusAction ( editorState, block, action ) ) )
+                } }
+            /> ) }
         </Menu.Items>
     </Menu>
 }
---
+++ BlockEditor/PlusMenuButton/plusActions.ts
@@ -0,0 +1,16 @@
+export interface plusAction {
+    label: string
+    action: string
+}
+
+const plusActions: plusAction [] = [
+    ...[ 'one', 'two', 'three', 'four', 'five', 'six' ].map ( ( numStr, n ) => ({
+        label: `Heading ${ n + 1 }`, action: `header-${ numStr }`
+    }) ),
+    { label: 'Quote', action: 'blockquote' },
+    { label: 'Code Block', action: 'code-block' },
+    { label: 'Ordered List', action: 'ordered-list-item' },
+    { label: 'Unordered List', action: 'unordered-list-item' },
+    { label: 'Paragraph', action: 'unstyled' },
+]
+export default plusActions
