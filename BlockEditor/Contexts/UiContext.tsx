0b2782c31602e566595d7f17b6b8293f56a08823
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-07-07 21:28:23 +0430

    CodeStyle Fix

--- BlockEditor/Contexts/UiContext.tsx
+++ BlockEditor/Contexts/UiContext.tsx
@@ -1,11 +1,11 @@
-// TODO: Organize order
-
 import { createContext, useContext, useState, useLayoutEffect, useRef, MutableRefObject } from 'react'
 import { ContentBlock } from 'draft-js'
 import Editor from '@draft-js-plugins/editor'
 import useEditorContext from './EditorContext'
 import { language, direction } from 'BlockEditor'
 
+
+type BlockRefs = MutableRefObject < { [ key: string ]: HTMLDivElement | null } >
 
 export interface BlockControlsInfo {
     hoveredBlockKey?: string
@@ -63,68 +63,39 @@ export interface InlineStyleMenuInfo {
     getSelectionRect?: () => DOMRect | null
 }
 
-type BlockRefs = MutableRefObject < { [ key: string ]: HTMLDivElement | null } >
-
 /**
  * General information regarding the Block Editor user interface.
  */
 export interface UiContext {
-    dir: direction
-    lang: language
+    dir: direction, lang: language
+    externalStyles: { [ key: string ]: string }
+
+    editorRef: MutableRefObject < Editor >
+    wrapperRef: MutableRefObject < HTMLDivElement >
+    innerWrapperRef: MutableRefObject < HTMLDivElement >
+    blockRefs: BlockRefs
+
     blockControlsInfo: BlockControlsInfo
     setBlockControlsInfo: SetState < BlockControlsInfo >
     plusMenuInfo: PlusMenuInfo
     setPlusMenuInfo: SetState < PlusMenuInfo >
     dragInfo: DragInfo
     setDragInfo: SetState < DragInfo >
-    inlineStyleMenuInfo: InlineStyleMenuInfo
-    editorRef: MutableRefObject < Editor >
-    wrapperRef: MutableRefObject < HTMLDivElement >
-    innerWrapperRef: MutableRefObject < HTMLDivElement >
-    blockRefs: BlockRefs
-    externalStyles: { [ key: string ]: string }
 
+    inlineStyleMenuInfo: InlineStyleMenuInfo
 }
 
 export const UiContext = createContext < UiContext > ( null )
 export const useUiContext = () => useContext ( UiContext )
 export default useUiContext
 
-export function UiContextProvider ({ styles, dir, lang, children }) {
+export function UiContextProvider ({ dir, lang, styles, children }) {
     const { editorState } = useEditorContext ()
-    const selectionState = editorState.getSelection ()
+
     const editorRef = useRef ()
     const wrapperRef = useRef < HTMLDivElement > ()
     const innerWrapperRef = useRef < HTMLDivElement > ()
     const blockRefs: BlockRefs = useRef ({})
-
-    const [ dragInfo, setDragInfo ] = useState ({
-        dragging: false,
-        isDraggingByHandle: false,
-        block: null, elem: null
-    })
-
-    const [ plusMenuInfo, setPlusMenuInfo ] = useState < PlusMenuInfo > ({})
-    if ( plusMenuInfo.openedBlock && (
-        ! selectionState.getHasFocus () ||
-        plusMenuInfo.openedBlock.getKey () !== selectionState.getAnchorKey () ||
-        plusMenuInfo.openedBlock.getKey () !== selectionState.getFocusKey  () ||
-        selectionState.getAnchorOffset () !== selectionState.getFocusOffset ()
-    ) ) setPlusMenuInfo ( prev => ({ ...prev, openedBlock: null }) )
-
-    const inlineStyleMenuInfo: InlineStyleMenuInfo = ( () => {
-        try {
-            const isOpen = selectionState.getHasFocus () && (
-                selectionState.getAnchorKey () !== selectionState.getFocusKey () ||
-                selectionState.getAnchorOffset () !== selectionState.getFocusOffset ()
-            )
-            const domSelection = isOpen ? window.getSelection () : null
-            const getSelectionRect = () => domSelection?.getRangeAt ( 0 ).getBoundingClientRect ()
-            return { isOpen, domSelection, getSelectionRect }
-        } catch {
-            return { isOpen: false, getSelectionRect: () => null }
-        }
-    } ) ()
 
     const [ blockControlsInfo, setBlockControlsInfo ] = useState < BlockControlsInfo > ({ hash: 0 })
     useLayoutEffect ( () => { // Set Block Controls on the first block initialy
@@ -160,6 +131,35 @@ export function UiContextProvider ({ styles, dir, lang, children }) {
             observer.disconnect ()
         }
     }, [] )
+
+    const [ plusMenuInfo, setPlusMenuInfo ] = useState < PlusMenuInfo > ({})
+    const selectionState = editorState.getSelection ()
+    if ( plusMenuInfo.openedBlock && (
+        ! selectionState.getHasFocus () ||
+        plusMenuInfo.openedBlock.getKey () !== selectionState.getAnchorKey () ||
+        plusMenuInfo.openedBlock.getKey () !== selectionState.getFocusKey  () ||
+        selectionState.getAnchorOffset () !== selectionState.getFocusOffset ()
+    ) ) setPlusMenuInfo ( prev => ({ ...prev, openedBlock: null }) )
+
+    const [ dragInfo, setDragInfo ] = useState ({
+        dragging: false,
+        isDraggingByHandle: false,
+        block: null, elem: null
+    })
+
+    const inlineStyleMenuInfo: InlineStyleMenuInfo = ( () => {
+        try {
+            const isOpen = selectionState.getHasFocus () && (
+                selectionState.getAnchorKey () !== selectionState.getFocusKey () ||
+                selectionState.getAnchorOffset () !== selectionState.getFocusOffset ()
+            )
+            const domSelection = isOpen ? window.getSelection () : null
+            const getSelectionRect = () => domSelection?.getRangeAt ( 0 ).getBoundingClientRect ()
+            return { isOpen, domSelection, getSelectionRect }
+        } catch {
+            return { isOpen: false, getSelectionRect: () => null }
+        }
+    } ) ()
 
     return <UiContext.Provider
         value = {{
