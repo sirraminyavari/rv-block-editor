f44d2eaf3d4d353366dcb49606956721ba4eebbf
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-07-08 11:28:32 +0430

    Compressed SVG Icons

--- BlockEditor/icons.tsx
+++ BlockEditor/icons.tsx
@@ -1,20 +1,10 @@
 import { IconBase } from 'react-icons'
 
 
-export const DragHandleIcon = () => <IconBase viewBox="0 0 12 20">
-    <g id="Group_991" data-name="Group 991" transform="translate(-1730.184 -132)">
-        <g id="drag-handle-line" transform="translate(1730.184 132)">
-            <path id="Path_165" data-name="Path 165" d="M17.5,12.5a2,2,0,1,1-2-2A2,2,0,0,1,17.5,12.5Z" transform="translate(-13.5 -10.5)" fill="currentColor" />
-            <path id="Path_166" data-name="Path 166" d="M17.5,24.5a2,2,0,1,1-2-2A2,2,0,0,1,17.5,24.5Z" transform="translate(-13.5 -6.5)" fill="currentColor" />
-            <path id="Path_167" data-name="Path 167" d="M23.5,12.5a2,2,0,1,1-2-2A2,2,0,0,1,23.5,12.5Z" transform="translate(-11.5 -10.5)" fill="currentColor" />
-            <path id="Path_168" data-name="Path 168" d="M23.5,24.5a2,2,0,1,1-2-2A2,2,0,0,1,23.5,24.5Z" transform="translate(-11.5 -6.5)" fill="currentColor" />
-            <path id="Path_169" data-name="Path 169" d="M23.5,18.5a2,2,0,1,1-2-2A2,2,0,0,1,23.5,18.5Z" transform="translate(-11.5 -8.5)" fill="currentColor" />
-            <path id="Path_170" data-name="Path 170" d="M17.5,18.5a2,2,0,1,1-2-2A2,2,0,0,1,17.5,18.5Z" transform="translate(-13.5 -8.5)" fill="currentColor" />
-        </g>
-    </g>
+export const DragHandleIcon = () => <IconBase viewBox = "0 0 12 20">
+    <path d = "M4 2a2 2 0 1 1-2-2 2 2 0 0 1 2 2zm0 16a2 2 0 1 1-2-2 2 2 0 0 1 2 2zm8-16a2 2 0 1 1-2-2 2 2 0 0 1 2 2zm0 16a2 2 0 1 1-2-2 2 2 0 0 1 2 2zm0-8a2 2 0 1 1-2-2 2 2 0 0 1 2 2zm-8 0a2 2 0 1 1-2-2 2 2 0 0 1 2 2z" />
 </IconBase>
 
-export const PlusIcon = () => <IconBase viewBox="0 0 32 32">
-    <path id="Path_218" data-name="Path 218" d="M25.665,17.473h-5.73v-5.73a1.231,1.231,0,1,0-2.462,0v5.73h-5.73A1.179,1.179,0,0,0,10.512,18.7a1.191,1.191,0,0,0,1.231,1.231h5.73v5.73A1.192,1.192,0,0,0,18.7,26.9a1.224,1.224,0,0,0,1.231-1.231v-5.73h5.73a1.231,1.231,0,1,0,0-2.462Z" transform="translate(-2.704 -2.704)" fill="currentColor" />
-    <path id="Path_219" data-name="Path 219" d="M19.375,5.529A13.839,13.839,0,1,1,9.582,9.582a13.758,13.758,0,0,1,9.793-4.053m0-2.154a16,16,0,1,0,16,16A16,16,0,0,0,19.375,3.375Z" transform="translate(-3.375 -3.375)" fill="currentColor" />
+export const PlusIcon = () => <IconBase viewBox = "0 0 32 32">
+    <path d = "M22.961 14.769h-5.73v-5.73a1.23 1.23 0 1 0-2.462 0v5.73h-5.73a1.18 1.18 0 0 0-1.231 1.227 1.19 1.19 0 0 0 1.231 1.231h5.73v5.73a1.19 1.19 0 0 0 1.227 1.239c.327.002.641-.127.873-.358a1.22 1.22 0 0 0 .358-.873v-5.73h5.73a1.23 1.23 0 1 0 0-2.462zM16 2.154a13.84 13.84 0 1 1-9.793 4.053A13.76 13.76 0 0 1 16 2.154M16 0C7.163 0 0 7.163 0 16s7.163 16 16 16 16-7.163 16-16S24.837 0 16 0z" />
 </IconBase>
--- Plugins/BasicInlineStyles/icons.tsx
+++ Plugins/BasicInlineStyles/icons.tsx
@@ -1,40 +1,18 @@
 import { IconBase } from 'react-icons'
 
 
-export const BoldIcon = () => <IconBase viewBox="0 0 15.855 18">
-    <g id="Group_2005" data-name="Group 2005">
-        <path id="Path_195" data-name="Path 195" d="M42.326,7.508A4.375,4.375,0,0,0,39.271,0H30.5V1.406h2.108V16.594H30.5V18H41.029a5.326,5.326,0,0,0,1.3-10.492Zm-8.311-6.1h5.256a2.971,2.971,0,0,1,0,5.941H34.015Zm7.015,15.188H34.015V8.754h7.015a3.92,3.92,0,0,1,0,7.84Z" transform="translate(-30.5 0)" fill="currentColor"/>
-    </g>
+export const BoldIcon = () => <IconBase viewBox = "0 0 15.855 18">
+    <path d = "M11.826 7.508c1.275-1.242 1.67-3.133.999-4.782S10.551-.001 8.771 0H0v1.406h2.108v15.188H0V18h10.529c2.69.002 4.959-2.002 5.29-4.671s-1.381-5.167-3.99-5.821zm-8.311-6.1h5.256a2.97 2.97 0 0 1 2.916 2.971 2.97 2.97 0 0 1-2.916 2.97H3.515zm7.015 15.188H3.515V8.754h7.015a3.92 3.92 0 1 1 0 7.84z" />
 </IconBase>
 
-export const ItalicIcon = () => <IconBase viewBox="0 0 9.176 18">
-    <g id="Group_2006" data-name="Group 2006">
-        <path id="Path_196" data-name="Path 196" d="M129.051,0V1.406h1.943l-3.25,15.188H125.5V18h5.625V16.594h-1.943l3.25-15.187h2.244V0Z" transform="translate(-125.5)" fill="currentColor"/>
-    </g>
+export const ItalicIcon = () => <IconBase viewBox = "0 0 9.176 18">
+    <path d = "M3.551 0v1.406h1.943l-3.25 15.188H0V18h5.625v-1.406H3.682l3.25-15.187h2.244V0z" />
 </IconBase>
 
-export const UnderlineIcon = () => <IconBase viewBox="0 0 18 18">
-    <g id="Group_2008" data-name="Group 2008" transform="translate(0 16.594)">
-        <g id="Group_2007" data-name="Group 2007">
-            <rect id="Rectangle_2654" data-name="Rectangle 2654" width="18" height="1.406" fill="currentColor" />
-        </g>
-    </g>
-    <g id="Group_2010" data-name="Group 2010" transform="translate(3.164)">
-        <g id="Group_2009" data-name="Group 2009">
-            <path id="Path_197" data-name="Path 197" d="M100.266,0V9.088a4.43,4.43,0,0,1-8.859,0V0H90V9.088a5.836,5.836,0,0,0,11.672,0V0Z" transform="translate(-90)" fill="currentColor" />
-        </g>
-    </g>
+export const UnderlineIcon = () => <IconBase viewBox = "0 0 18 18">
+    <path d = "M0 16.594h18V18H0zM13.43 0v9.088a4.43 4.43 0 0 1-8.859 0V0H3.164v9.088A5.84 5.84 0 0 0 9 14.924a5.84 5.84 0 0 0 5.836-5.836V0z" />
 </IconBase>
 
-export const StrikethroughIcon = () => <IconBase viewBox="0 0 18 18">
-    <g id="Group_2036" data-name="Group 2036">
-        <g id="Group_2035" data-name="Group 2035">
-            <path id="Path_206" data-name="Path 206" d="M9.663,8.3,9.343,8.13c-1.727-.9-3.156-1.816-3.156-3.912a2.813,2.813,0,0,1,5.625,0v.7h1.406v-.7a4.219,4.219,0,0,0-8.437,0A4.66,4.66,0,0,0,6.059,7.553,6.327,6.327,0,0,0,6.9,8.3H0V9.7H18V8.3Z" fill="currentColor" />
-        </g>
-    </g>
-    <g id="Group_2038" data-name="Group 2038" transform="translate(4.113 11.109)">
-        <g id="Group_2037" data-name="Group 2037">
-            <path id="Path_207" data-name="Path 207" d="M126.253,316h-1.626a3.445,3.445,0,1,1-6.221,2.039v-.7H117v.7A4.852,4.852,0,1,0,126.253,316Z" transform="translate(-117 -316)" fill="currentColor" />
-        </g>
-    </g>
+export const StrikethroughIcon = () => <IconBase viewBox = "0 0 18 18">
+    <path d = "M9.663 8.3l-.32-.17c-1.727-.9-3.156-1.816-3.156-3.912a2.813 2.813 0 0 1 5.625 0v.7h1.406v-.7a4.219 4.219 0 0 0-8.437 0 4.66 4.66 0 0 0 1.278 3.335A6.327 6.327 0 0 0 6.9 8.3H0v1.4h18V8.3zm3.703 2.809H11.74c.885 1.205.893 2.843.018 4.055s-2.431 1.723-3.853 1.263-2.386-1.784-2.386-3.279v-.7H4.113v.7a4.85 4.85 0 1 0 9.253-2.039z" />
 </IconBase>
