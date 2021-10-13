af51d967de14e4b7fc3cab4b3840bd476fcccd33
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-10-13 17:25:05 +0330

    Nest a block in-place using DnD

--- BlockEditor/Lib/moveBlockRange.ts
+++ BlockEditor/Lib/moveBlockRange.ts
@@ -14,12 +14,27 @@ export default function moveBlockRange (
     const range = getBlockRange ( blockMap, startKey, endKey )
     const withoutRangeBlockMap = removeBlockRange ( blockMap, startKey, endKey )
 
-    const before = withoutRangeBlockMap.takeUntil ( ( _, key ) => key === targetKey )
-    const after = withoutRangeBlockMap.skipUntil ( ( _, key ) => key === targetKey )
+    const { adjustedTargetKey, adjustedInsertionMode } = ( () => {
+        const isInplace = range.some ( b => b.getKey () === targetKey )
+        if ( ! isInplace ) return { adjustedTargetKey: targetKey, adjustedInsertionMode: insertionMode }
+
+        const firstInRangeKey = range.first ().getKey ()
+        const blockBefore = blockMap.takeUntil ( b => b.getKey () === firstInRangeKey ).last ()
+        if ( blockBefore ) return { adjustedTargetKey: blockBefore.getKey (), adjustedInsertionMode: 'after' }
+
+        const lastInRangeKey = range.last ().getKey ()
+        const blockAfter = blockMap.skipUntil ( b => b.getKey () === lastInRangeKey ).skip ( 1 ).first ()
+        if ( blockAfter ) return { adjustedTargetKey: blockAfter.getKey (), adjustedInsertionMode: 'before' }
+
+        throw new Error ( "Illegal: Moving the whole content" )
+    } ) ()
+
+    const before = withoutRangeBlockMap.takeUntil ( ( _, key ) => key === adjustedTargetKey )
+    const after = withoutRangeBlockMap.skipUntil ( ( _, key ) => key === adjustedTargetKey )
 
     const borderBlock = after.first ()
-    const adjustedBefore = insertionMode === 'before' ? before : before.concat ([[ borderBlock.getKey (), borderBlock ]])
-    const adjustedAfter = insertionMode === 'before' ? after : after.skip ( 1 )
+    const adjustedBefore = adjustedInsertionMode === 'before' ? before : before.concat ([[ borderBlock.getKey (), borderBlock ]])
+    const adjustedAfter = adjustedInsertionMode === 'before' ? after : after.skip ( 1 )
 
     const newBlockMap = adjustedBefore.concat ( range, adjustedAfter ).toOrderedMap ()
     return newBlockMap
