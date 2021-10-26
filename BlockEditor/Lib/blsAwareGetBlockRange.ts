a19545f123cb020c6bf331d60bbec742fb2784c0
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-10-26 14:16:41 +0330

    BugFix

--- BlockEditor/Lib/blsAwareGetBlockRange.ts
+++ BlockEditor/Lib/blsAwareGetBlockRange.ts
@@ -1,7 +1,6 @@
 import { BlockMap } from 'draft-js'
 
 import getSelectionDepth from 'BlockEditor/Lib/getSelectionDepth'
-import trimCollapsedBlocks from 'BlockEditor/Lib/trimCollapsedBlocks'
 import getFirstAncestorByDepth from 'BlockEditor/Lib/getFirstAncestorByDepth'
 import getLastCousinShallowerThan from 'BlockEditor/Lib/getLastCousinShallowerThan'
 import getBlockRange from 'BlockEditor/Lib/getBlockRange'
@@ -13,11 +12,7 @@ export default function blsAwareGetBlockRange (
     endKey: string,
     selectionDepth: number = getSelectionDepth ( blockMap, startKey, endKey )
 ): BlockMap {
-    const trimmedBlockMap = trimCollapsedBlocks ( blockMap )
-
-    const adjustedStartKey = getFirstAncestorByDepth ( trimmedBlockMap, startKey, selectionDepth ).getKey ()
-    const adjustedEndKey = getLastCousinShallowerThan ( trimmedBlockMap, endKey, selectionDepth ).getKey ()
-
-    const selectedBlocks = getBlockRange ( blockMap, adjustedStartKey, adjustedEndKey )
-    return selectedBlocks
+    const adjustedStartKey = getFirstAncestorByDepth ( blockMap, startKey, selectionDepth ).getKey ()
+    const adjustedEndKey = getLastCousinShallowerThan ( blockMap, endKey, selectionDepth ).getKey ()
+    return getBlockRange ( blockMap, adjustedStartKey, adjustedEndKey )
 }
