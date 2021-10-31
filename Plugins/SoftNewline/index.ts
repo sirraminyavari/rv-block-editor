04f322539c2b85c1113988082a482913a5677a07
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-10-31 20:47:33 +0330

    BugFix

--- Plugins/SoftNewline/index.ts
+++ Plugins/SoftNewline/index.ts
@@ -1,12 +1,37 @@
-import { EditorPlugin } from 'BlockEditor'
+import { EditorState, RichUtils, Modifier } from 'draft-js'
+import isSoftNewlineEvent from 'draft-js/lib/isSoftNewlineEvent'
 
-import createSoftNewLinePlugin from '@jimmycode/draft-js-soft-newline-plugin'
+import { EditorPlugin } from 'BlockEditor'
 
 
 export default function createSoftNewlinePlugin (): EditorPlugin {
     return {
         id: 'softnewline',
 
-        ...createSoftNewLinePlugin ()
+        handleReturn ( event, editorState, { setEditorState } ) {
+            if ( ! isSoftNewlineEvent ( event ) ) return 'not-handled'
+
+            const selectionState = editorState.getSelection()
+            if ( selectionState.isCollapsed () ) {
+                setEditorState ( RichUtils.insertSoftNewline ( editorState ) )
+                return 'handled'
+            }
+
+            const contentState = editorState.getCurrentContent ()
+            const contentWithoutSelectedText = Modifier.removeRange ( contentState, selectionState, 'forward' )
+            const newSelection = contentWithoutSelectedText.getSelectionAfter ()
+            const newBlock = contentWithoutSelectedText.getBlockForKey ( newSelection.getStartKey () )
+
+            const newContent = Modifier.insertText (
+                contentWithoutSelectedText,
+                newSelection,
+                '\n',
+                newBlock.getInlineStyleAt ( newSelection.getStartOffset () ),
+                null
+            )
+
+            setEditorState ( EditorState.push ( editorState, newContent, 'insert-fragment' ) )
+            return 'handled'
+        }
     }
 }
