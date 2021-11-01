389f786b92c5d40f34ed26f32b5e8d8ac999ae80
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-11-01 17:57:10 +0330

    Refactor & Improvements

--- Plugins/TextAnnotations/index.tsx
+++ Plugins/TextAnnotations/index.tsx
@@ -1,92 +1,57 @@
-import { EditorState, Modifier, CompositeDecorator } from 'draft-js'
+import { CompositeDecorator } from 'draft-js'
 
-import { EditorPlugin, DecoratorComponent } from 'BlockEditor'
+import { EditorPlugin } from 'BlockEditor'
 import makeEntityStrategy from 'BlockEditor/Utils/makeEntityStrategy'
 
-import { FC } from 'react'
-import { InlineStyleComponentProps } from 'BlockEditor'
-import Button from 'BlockEditor/Ui/Button'
-import { TextColorIcon, HighlightColorIcon } from './Icons'
-import styles from './styles.module.scss'
+import getColorSelectComponent from './ColorSelect'
+import getDecoratorComponent from './DecoratorComponent'
+import { TextColorIcon, HighlightColorIcon } from './icons'
 
+
+const textColors = {
+    red: { color: 'red' },
+    green: { color: 'green' },
+    blue: { color: 'blue' }
+}
+
+const highlightColors = {
+    red: { backgroundColor: 'red', color: 'white' },
+    green: { backgroundColor: 'green', color: 'white' },
+    blue: { backgroundColor: 'blue', color: 'white' }
+}
 
 export default function createTextAnnotationsPlugin (): EditorPlugin {
     return {
-        id: 'links',
+        id: 'text-annotations',
 
         inlineStyles: [
-            { Component: getColorSelectComponent ( 'TEXT-COLOR' ) },
-            { Component: getColorSelectComponent ( 'HIGHLIHGT-COLOR' ) }
+            { Component: getColorSelectComponent ({
+                entityName: 'TEXT-COLOR',
+                Icon: TextColorIcon,
+                colors: Object.keys ( textColors )
+            }) },
+            { Component: getColorSelectComponent ({
+                entityName: 'HIGHLIHGT-COLOR',
+                Icon: HighlightColorIcon,
+                colors: Object.keys ( highlightColors )
+            }) }
         ],
 
         decorators: [
             new CompositeDecorator ([
                 {
                     strategy: makeEntityStrategy ( 'TEXT-COLOR' ),
-                    component: TextColor
+                    component: getDecoratorComponent ({
+                        getStyles: color => textColors [ color ]
+                    })
                 },
                 {
                     strategy: makeEntityStrategy ( 'HIGHLIHGT-COLOR' ),
-                    component: HighlightColor
+                    component: getDecoratorComponent ({
+                        getStyles: color => highlightColors [ color ]
+                    })
                 }
             ])
         ]
     }
-}
-
-
-function getColorSelectComponent ( entityName: string ) {
-    return props => <ColorSelect entityName = { entityName } { ...props } />
-}
-
-export interface ColorSelectProps extends InlineStyleComponentProps {
-    entityName: string
-}
-
-const ColorSelect: FC < ColorSelectProps > = ({ entityName, editorState, setEditorState }) => {
-    return <div className = { styles.colorSelectWrapper }>
-        <Button Icon = { TextColorIcon } />
-        <div className = { styles.colors }>
-            { [ 'red', 'green', 'blue' ].map ( color => <Button
-                key = { color }
-                Icon = { TextColorIcon }
-                style = {{ backgroundColor: color }}
-                onClick = { () => {
-                    const contentState = editorState.getCurrentContent ()
-                    const selectionState = editorState.getSelection ()
-                    const contentStateWithEntity = contentState.createEntity ( entityName, 'MUTABLE', { color } )
-                    const entityKey = contentStateWithEntity.getLastCreatedEntityKey ()
-                    const contentStateWithLink = Modifier.applyEntity (
-                        contentStateWithEntity,
-                        selectionState,
-                        entityKey
-                    )
-                    const newEditorState = EditorState.set ( editorState, {
-                        currentContent: contentStateWithLink
-                    } )
-                    setEditorState ( newEditorState )
-                } }
-            /> ) }
-        </div>
-    </div>
-}
-
-const TextColor: DecoratorComponent = ({ contentState, entityKey, children }) => {
-    const entity = contentState.getEntity ( entityKey )
-    if ( ! entity ) return children
-    const { color } = entity.getData ()
-    return <span
-        style = {{ color }}
-        children = { children }
-    />
-}
-
-const HighlightColor: DecoratorComponent = ({ contentState, entityKey, children }) => {
-    const entity = contentState.getEntity ( entityKey )
-    if ( ! entity ) return children
-    const { color } = entity.getData ()
-    return <span
-        style = {{ backgroundColor: color }}
-        children = { children }
-    />
 }
