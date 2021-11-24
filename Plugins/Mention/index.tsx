580e297d2f72e33397b6f6193de2f21213420e00
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-11-24 21:09:01 +0330

    Fix

--- Plugins/Mention/index.tsx
+++ Plugins/Mention/index.tsx
@@ -33,12 +33,12 @@ export default function createMentionPlugin ( { mentions }: Config ): EditorPlug
 
         decorators: [ new CompositeDecorator ( _plugin.decorators as any ) ],
 
-        OverlayComponent: () => {
-            const { editorState } = useEditorContext ()
-            const a = editorState.getCurrentContent ().getBlockMap ().size
-            return <div className = { styles.overlay }>
-                <h1>Silam { a }</h1>
-            </div>
-        }
+        // OverlayComponent: () => {
+        //     const { editorState } = useEditorContext ()
+        //     const a = editorState.getCurrentContent ().getBlockMap ().size
+        //     return <div className = { styles.overlay }>
+        //         <h1>Silam { a }</h1>
+        //     </div>
+        // }
     }
 }
