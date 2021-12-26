cb84a32604fcb2192e81399a93c320aa3864a7ce
Arvin Nabavi <aakhavannabavi@gmail.com>
2021-12-26 12:05:34 +0330

    Imps

--- Plugins/BlockAlignment/AlignmentButtons.tsx
+++ Plugins/BlockAlignment/AlignmentButtons.tsx
@@ -3,15 +3,20 @@ import Button from 'BlockEditor/Ui/Button'
 
 import setBlockAlignment from './setBlockAlignment'
 
+import { AlignLeftIcon, AlignCenterIcon, AlignRightIcon } from './icons'
 import styles from './styles.module.scss'
 
+
+const icons = {
+    left: AlignLeftIcon, center: AlignCenterIcon, right: AlignRightIcon
+}
 
 const AlignmentButtons: InlineStyleComponent = ({ editorState, setEditorState }) => {
     return <div className = { styles.buttonWrapper }>
         { Object.values ( Alignment ).map ( alignmentVal => <Button
             key = { alignmentVal }
             onClick = { () => setBlockAlignment ( alignmentVal, editorState, setEditorState ) }
-            children = { alignmentVal }
+            Icon = { icons [ alignmentVal ] }
         /> ) }
     </div>
 }
