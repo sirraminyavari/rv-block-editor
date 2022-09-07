beade88abb5a39b87e1a5f1c2357616ff321c725
soren abedi <soren.abedi@gmail.com>
2022-09-07 10:55:13 +0430

    feat: :sparkles: implement **stopPropagation** for `Link` and `Mention` plugins
    Signed-off-by: soren abedi <soren.abedi@gmail.com>

--- src/Plugins/Mention/MentionComponent.tsx
+++ src/Plugins/Mention/MentionComponent.tsx
@@ -1,10 +1,15 @@
-import { FC } from 'react'
+import type { FC, MouseEventHandler } from 'react'
 
 import { SubMentionComponentProps } from '@draft-js-plugins/mention/lib/Mention.d'
 
-const MentionComponent: FC<SubMentionComponentProps> = ({ mention, children }) => (
-    <a href={mention.link}>
-        <span children={children} />
-    </a>
-)
+const MentionComponent: FC<SubMentionComponentProps> = ({ mention, children }) => {
+    const onClick: MouseEventHandler<HTMLAnchorElement> = event => {
+        event.stopPropagation()
+    }
+    return (
+        <a href={mention.link} onClick={onClick} target="_blank">
+            <span children={children} />
+        </a>
+    )
+}
 export default MentionComponent
