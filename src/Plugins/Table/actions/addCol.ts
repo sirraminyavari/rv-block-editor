660c21c5d73fbbced4c287066138093a2bc05c7c
Arvin Nabavi <aakhavannabavi@gmail.com>
2022-10-07 18:52:19 +0330

    BugFix

--- src/Plugins/Table/actions/addCol.ts
+++ src/Plugins/Table/actions/addCol.ts
@@ -1,4 +1,5 @@
 import { EditorState, ContentState, SelectionState, ContentBlock, Modifier } from 'draft-js'
+import { Alignment } from 'BlockEditor'
 import _ from 'lodash'
 import mergeBlockData from 'BlockEditor/Lib/mergeBlockData'
 import tableLib from '../lib'
@@ -6,7 +7,7 @@ import tableLib from '../lib'
 import { TABLE_CELL_MARKER } from '..'
 
 export function addCol(contentState: ContentState, tableBlock: ContentBlock, anchorCol: number) {
-    const { blockKey, rowN, colN } = tableLib.getTableData(tableBlock)
+    const { blockKey, rowN, colN, alignments } = tableLib.getTableData(tableBlock)
 
     const eoColOffsets = (() => {
         const segments = tableBlock.getText().split(TABLE_CELL_MARKER.end)
@@ -22,14 +23,36 @@ export function addCol(contentState: ContentState, tableBlock: ContentBlock, anc
         return absoluteOffsets
     })()
 
-    // Reverse the offsets so deletions wont affect their correctness
-    const newContentState = eoColOffsets.reverse().reduce((contentState, offset) => {
-        return Modifier.insertText(
-            contentState,
-            SelectionState.createEmpty(blockKey).merge({ anchorOffset: offset, focusOffset: offset }),
-            `${TABLE_CELL_MARKER.start}${TABLE_CELL_MARKER.end}`
+    const newContentState = _.chain(contentState)
+        .thru(contentState =>
+            mergeBlockData(EditorState.createWithContent(contentState), blockKey, {
+                colN: colN + 1,
+                alignments: adjustAlignments(alignments, anchorCol, rowN, colN),
+            }).getCurrentContent()
         )
-    }, mergeBlockData(EditorState.createWithContent(contentState), blockKey, { colN: colN + 1 }).getCurrentContent())
+        // Reverse the offsets so deletions wont affect their correctness
+        .thru(contentState =>
+            eoColOffsets.reverse().reduce((contentState, offset) => {
+                return Modifier.insertText(
+                    contentState,
+                    SelectionState.createEmpty(blockKey).merge({ anchorOffset: offset, focusOffset: offset }),
+                    `${TABLE_CELL_MARKER.start}${TABLE_CELL_MARKER.end}`
+                )
+            }, contentState)
+        )
+        .value()
 
     return newContentState
+}
+
+function adjustAlignments(alignments: Record<number, Alignment>, anchorCol: number, rowN: number, colN: number) {
+    const newAlignments: Record<number, Alignment> = {}
+    const modAnchor = anchorCol % colN
+    for (let n = 0, c = 0, l = rowN * colN; n < l; n++) {
+        const modN = n % colN
+        const currentAlign = alignments[n]
+        if (currentAlign) newAlignments[n + c] = currentAlign
+        if (modN === modAnchor) c++
+    }
+    return newAlignments
 }
--- src/Plugins/Table/lib/getTableData.ts
+++ src/Plugins/Table/lib/getTableData.ts
@@ -1,9 +1,12 @@
 import { ContentBlock } from 'draft-js'
+import { Alignment } from 'BlockEditor'
+import getObjData from 'BlockEditor/Lib/getObjData'
 
 export function getTableData(tableBlock: ContentBlock) {
     const blockKey = tableBlock.getKey()
     const data = tableBlock.getData()
-    const rowN = data.get('rowN') as number
-    const colN = data.get('colN') as number
-    return { blockKey, data, rowN, colN }
+    const rowN: number = data.get('rowN')
+    const colN: number = data.get('colN')
+    const alignments: Record<number, Alignment> = getObjData(data, 'alignments') || {}
+    return { blockKey, data, rowN, colN, alignments }
 }
